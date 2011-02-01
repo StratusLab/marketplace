046f33a4e12bca97be5879eb43e39ae18defa245
Charles Loomis <loomis@lal.in2p3.fr>
2011-02-01 15:54:20 +0100

    move metadata utilities into this repository

---
+++ metadata/src/main/java/eu/stratuslab/metadata/ValidateSign.java
@@ -0,0 +1,148 @@
+package eu.stratuslab.metadata;
+
+import javax.xml.crypto.*;
+import javax.xml.crypto.dsig.*;
+import javax.xml.crypto.dom.*;
+import javax.xml.crypto.dsig.dom.DOMValidateContext;
+import javax.xml.crypto.dsig.keyinfo.*;
+import java.io.FileInputStream;
+import java.security.*;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import java.security.cert.*;
+/**
+ * This is a simple example of validating an XML 
+ * Signature using the JSR 105 API. It assumes the key needed to
+ * validate the signature is contained in a KeyValue KeyInfo. 
+ */
+public class ValidateSign {
+
+    //
+    // Synopsis: java Validate [document]
+    //
+    //	  where "document" is the name of a file containing the XML document
+    //	  to be validated.
+    //
+    public static void main(String[] args) throws Exception {
+
+	// Instantiate the document to be validated
+	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	dbf.setNamespaceAware(true);
+	Document doc =
+            dbf.newDocumentBuilder().parse(new FileInputStream(args[0]));
+
+	// Find Signature element
+	NodeList nl = 
+	    doc.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
+	if (nl.getLength() == 0) {
+	    throw new Exception("Cannot find Signature element");
+	}
+
+	// Create a DOM XMLSignatureFactory that will be used to unmarshal the 
+	// document containing the XMLSignature 
+	XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+
+	// Create a DOMValidateContext and specify a KeyValue KeySelector
+        // and document context
+	DOMValidateContext valContext = new DOMValidateContext(new X509KeySelector(), nl.item(0));
+	
+	// unmarshal the XMLSignature
+	XMLSignature signature = fac.unmarshalXMLSignature(valContext);
+
+	// Validate the XMLSignature (generated above)
+	boolean coreValidity = signature.validate(valContext); 
+
+	// Check core validation status
+	if (coreValidity == false) {
+    	    System.err.println("Signature failed core validation"); 
+	    boolean sv = signature.getSignatureValue().validate(valContext);
+	    System.out.println("signature validation status: " + sv);
+	    // check the validation status of each Reference
+	    Iterator i = signature.getSignedInfo().getReferences().iterator();
+	    for (int j=0; i.hasNext(); j++) {
+		boolean refValid = 
+		    ((Reference) i.next()).validate(valContext);
+		System.out.println("ref["+j+"] validity status: " + refValid);
+	    }
+	} else {
+    	    System.out.println("Signature passed core validation");
+	}
+    }
+
+    /**
+     * KeySelector which retrieves the public key out of the
+     * KeyValue element and returns it.
+     * NOTE: If the key algorithm doesn't match signature algorithm,
+     * then the public key will be ignored.
+     */
+        public static class X509KeySelector extends KeySelector {
+    public KeySelectorResult select(KeyInfo keyInfo,
+                                    KeySelector.Purpose purpose,
+                                    AlgorithmMethod method,
+                                    XMLCryptoContext context)
+        throws KeySelectorException {
+        Iterator ki = keyInfo.getContent().iterator();
+	PublicKey key =null;
+        while (ki.hasNext()) {
+            XMLStructure info = (XMLStructure) ki.next();
+            if (!(info instanceof X509Data)&&!(info instanceof KeyValue))
+                continue;
+	    if (info instanceof X509Data)
+	    {
+            X509Data x509Data = (X509Data) info;
+            Iterator xi = x509Data.getContent().iterator();
+            while (xi.hasNext()) {
+                Object o = xi.next();
+                if (!(o instanceof X509Certificate))
+                    continue;
+                System.out.println("Signed by : "+ ((X509Certificate)o).getSubjectX500Principal().getName()); 
+                key = ((X509Certificate)o).getPublicKey();
+		}
+		}
+                if (info instanceof KeyValue){
+		try { 
+ 		 key = ((KeyValue)info).getPublicKey();
+		}catch (KeyException ke) {
+                        throw new KeySelectorException(ke);
+                    }		
+		} 
+                // Make sure the algorithm is compatible
+                // with the method.
+                if (algEquals(method.getAlgorithm(), key.getAlgorithm())) {
+                return new SimpleKeySelectorResult(key);
+		}
+            }
+       
+        throw new KeySelectorException("No key found!");
+    }
+
+    static boolean algEquals(String algURI, String algName) {
+        if ((algName.equalsIgnoreCase("DSA") &&
+            algURI.equalsIgnoreCase(SignatureMethod.DSA_SHA1)) ||
+            (algName.equalsIgnoreCase("RSA") &&
+            algURI.equalsIgnoreCase(SignatureMethod.RSA_SHA1))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+private static class SimpleKeySelectorResult implements KeySelectorResult {
+        private PublicKey pk;
+        SimpleKeySelectorResult(PublicKey pk) {
+            this.pk = pk;
+        }
+
+        public Key getKey() { return pk; }
+    }
+    }
+
+
+
+
+
