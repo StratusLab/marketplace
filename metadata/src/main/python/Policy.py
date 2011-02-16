b00fc8321195e99263ab92171d2ec8fe23630766
Marc-Elian Begin <meb@sixsq.com>
2011-02-16 16:10:14 +0100

    Minor refactor and removed tabs

--- metadata/src/main/python/Policy.py
+++ metadata/src/main/python/Policy.py
@@ -1,4 +1,25 @@
-from xml.etree.ElementTree import ElementTree
+try:
+    from lxml import etree
+except ImportError:
+    try:
+        # Python 2.5
+        import xml.etree.cElementTree as etree
+    except ImportError:
+        try:
+            # Python 2.5
+            import xml.etree.ElementTree as etree
+        except ImportError:
+            try:
+                # normal cElementTree install
+                import cElementTree as etree
+            except ImportError:
+                try:
+                    # normal ElementTree install
+                    import elementtree.ElementTree as etree
+                except ImportError:
+                    raise Exception("Failed to import ElementTree from any known place")
+
+import ConfigParser
 
 
 # keep: inputs: element tree
@@ -6,12 +27,13 @@ from xml.etree.ElementTree import ElementTree
 #	return true email endorser listed in WhiteListEndorsers and checksum value is not listed in BlackListChecksums
 
 def keep(xmltree):
-    emailendorser = xmltree.findtext(".//{http://mp.stratuslab.eu/slreq#}endorser/{http://mp.stratuslab.eu/slreq#}email")
-    checksumimage = xmltree.findtext(".//{http://mp.stratuslab.eu/slreq#}checksum/{http://mp.stratuslab.eu/slreq#}value")
-    if emailendorser in WhiteListEndorsers and checksumimage not in BlackListChecksums:
-	return True
+    xpathPrefix = './/{http://mp.stratuslab.eu/slreq#}endorser/{http://mp.stratuslab.eu/slreq#}'
+    emailendorser = xmltree.findtext(xpathPrefix + 'email')
+    checksumimage = xmltree.findtext(xpathPrefix + 'value')
+    if (emailendorser in WhiteListEndorsers) and (checksumimage not in BlackListChecksums):
+        return True
     else:
-	return False
+        return False
 
 #filter: inputs: metadatas list as elementt ree.
 #	 remove unwanted element tree from metadatas list
@@ -19,21 +41,18 @@ def keep(xmltree):
 
 def filter(xmltrees):
     for xmltree in xmltrees:
-	if keep(xmltree) == False:
-	xmltrees.remove(xmltree)
+        if keep(xmltree) == False:
+            xmltrees.remove(xmltree)
     return xmltrees
-
-
 
 # init : input : site config file policy
 #	 read config file policy, constructs:  WhiteListEndorsers, WhiteListEndorsers and BlackListChecksums
 
 def init(configfile):
-    import ConfigParser
     config = ConfigParser.ConfigParser()
     config.read(configfile)
     for i,j in config.items('whitelistendorsers'):
-	WhiteListEndorsers.append(j)
+        WhiteListEndorsers.append(j)
     for i,j in config.items('blacklistendorsers'):
         WhiteListEndorsers.append(j)
     for i,j in config.items('blacklistchecksums'):
