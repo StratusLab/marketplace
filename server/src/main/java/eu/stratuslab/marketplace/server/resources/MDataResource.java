d1ebfbfe18bb6dafc0d41b88ee0a0d7a2f8b2421
Stuart Kenny <stuart.kenny@scss.tcd.ie>
2011-02-25 16:14:11 +0000

    Accept signed metadata only, and add some simple HTML output

--- server/src/main/java/eu/stratuslab/marketplace/server/resources/MDataResource.java
+++ server/src/main/java/eu/stratuslab/marketplace/server/resources/MDataResource.java
@@ -15,15 +15,20 @@ import javax.xml.parsers.DocumentBuilder;
 import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
+import org.restlet.ext.xml.DomRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import eu.stratuslab.marketplace.XMLUtils;
+import eu.stratuslab.marketplace.metadata.MetadataException;
 import eu.stratuslab.marketplace.metadata.MetadataUtils;
+import eu.stratuslab.marketplace.metadata.ValidateMetadataConstraints;
+import eu.stratuslab.marketplace.metadata.ValidateXMLSignature;
 
 /**
  * This resource represents a list of all Metadata entries
@@ -52,37 +57,115 @@ public class MDataResource extends BaseResource {
 			e.printStackTrace();
 		}
         
-		String identifier = extractTextContent(datumDoc, DCTERMS_NS_URI, "identifier");
-		String endorser = extractTextContent(datumDoc, SLREQ_NS_URI, "email");
-		String created = extractTextContent(datumDoc, DCTERMS_NS_URI, "created");
-		         
-        String ref = getRequest().getResourceRef().toString();
-        String iri = ref + "/" + identifier + "/" + endorser + "/" + created;
+		if(datumDoc != null){
+			try {
+				ValidateXMLSignature.validate(datumDoc);
+				ValidateMetadataConstraints.validate(datumDoc);
+				
+				String identifier = extractTextContent(datumDoc, DCTERMS_NS_URI, "identifier");
+				String endorser = extractTextContent(datumDoc, SLREQ_NS_URI, "email");
+				String created = extractTextContent(datumDoc, DCTERMS_NS_URI, "created");
 
-        File rdfFileParent = new File(getDataDir(), identifier + File.separatorChar + endorser);
-        File rdfFile = new File(rdfFileParent, created + ".xml");
-        
-        if(!rdfFileParent.exists()){
-        	if(rdfFileParent.mkdirs()){
-        		MetadataUtils.writeStringToFile(rdfEntry, rdfFile);	
-        	}
-        } else {
-        	MetadataUtils.writeStringToFile(XMLUtils.documentToString(datumDoc), rdfFile);
-        }
-                
-        storeMetadatum(iri, rdfEntry);
-        // Set the response's status and entity
-        setStatus(Status.SUCCESS_CREATED);
-        Representation rep = new StringRepresentation("Metadata entry created",
-            MediaType.TEXT_PLAIN);
-        // Indicates where is located the new resource.
-        rep.setLocationRef(getRequest().getResourceRef().getIdentifier() + "/"
-             + identifier + "/" + endorser + "/" + created);
-        result = rep;
+				String ref = getRequest().getResourceRef().toString();
+				String iri = ref + "/" + identifier + "/" + endorser + "/" + created;
 
+				File rdfFileParent = new File(getDataDir(), identifier + File.separatorChar + endorser);
+				File rdfFile = new File(rdfFileParent, created + ".xml");
+
+				if(!rdfFileParent.exists()){
+					if(rdfFileParent.mkdirs()){
+						MetadataUtils.writeStringToFile(rdfEntry, rdfFile);	
+					}
+				} else {
+					MetadataUtils.writeStringToFile(XMLUtils.documentToString(datumDoc), rdfFile);
+				}
+
+				storeMetadatum(iri, rdfEntry);
+				// Set the response's status and entity
+				setStatus(Status.SUCCESS_CREATED);
+				Representation rep = new StringRepresentation("Metadata entry created",
+						MediaType.TEXT_PLAIN);
+				// Indicates where is located the new resource.
+				rep.setLocationRef(getRequest().getResourceRef().getIdentifier() + "/"
+						+ identifier + "/" + endorser + "/" + created);
+				result = rep;
+
+			} catch (MetadataException m){
+				setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+	            result = generateErrorRepresentation(m.getMessage(), "1");
+			}
+		}
+						
         return result;
     }
         
+    /**
+     * Generate an XML representation of an error response.
+     * 
+     * @param errorMessage
+     *            the error message.
+     * @param errorCode
+     *            the error code.
+     */
+    private Representation generateErrorRepresentation(String errorMessage,
+            String errorCode) {
+        DomRepresentation result = null;
+        // This is an error
+        // Generate the output representation
+        try {
+            result = new DomRepresentation(MediaType.TEXT_XML);
+            // Generate a DOM document representing the list of
+            // items.
+            Document d = result.getDocument();
+
+            Element eltError = d.createElement("error");
+
+            Element eltCode = d.createElement("code");
+            eltCode.appendChild(d.createTextNode(errorCode));
+            eltError.appendChild(eltCode);
+
+            Element eltMessage = d.createElement("message");
+            eltMessage.appendChild(d.createTextNode(errorMessage));
+            eltError.appendChild(eltMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+    
+    @Get("html")
+    public Representation toHtml() {
+    	String base = getRequest().getResourceRef().toString();
+    	if(base.endsWith("/")){
+    		base = base.substring(0, base.length() - 1);
+    	}
+    	String[] uris = getMetadata();
+    	StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("<html>");
+        stringBuilder
+                .append("<head><title>Metadata</title></head>");
+        stringBuilder.append("<body bgcolor=white>");
+
+        stringBuilder.append("<table border=\"0\">");
+        for (String uri : uris){
+            stringBuilder.append("<tr>");
+            stringBuilder.append("<td>");
+            stringBuilder.append("<a href=\"" + base + "/" + uri + "\">" + uri + "</a>");
+            stringBuilder.append("</td>");
+            stringBuilder.append("</tr>");
+        }
+        stringBuilder.append("</table>");
+        stringBuilder.append("</body>");
+        stringBuilder.append("</html>");
+
+        Representation representation = (new StringRepresentation(stringBuilder
+                .toString(), MediaType.TEXT_HTML));
+        
+        return representation;
+    }
+    
     /**
      * Returns a listing of all registered metadata or a particular entry if specified.
      */
@@ -93,7 +176,7 @@ public class MDataResource extends BaseResource {
         
         if(uris.length > 0){
         	for(String uri : uris ){
-        		String datum = getMetadatum(uri);
+        		String datum = getMetadatum(getDataDir() + File.separatorChar + uri + ".xml");
         		if(datum.startsWith(XML_HEADER)){
         			datum = datum.substring(XML_HEADER.length());
         		}
@@ -175,7 +258,7 @@ public class MDataResource extends BaseResource {
                 String iri = resultRow.get("identifier") 
                 + "/" + resultRow.get("email") + "/" 
                 + resultRow.get("created");
-                uris[i] = getDataDir() + File.separatorChar + iri + ".xml";
+                uris[i] = iri;
                 i++;
             }
                        
--- server/src/main/java/eu/stratuslab/marketplace/server/resources/MDatumResource.java
+++ server/src/main/java/eu/stratuslab/marketplace/server/resources/MDatumResource.java
@@ -1,5 +1,13 @@
 package eu.stratuslab.marketplace.server.resources;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+
 import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
@@ -15,8 +23,8 @@ public class MDatumResource extends BaseResource {
     
     @Override
     protected void doInit() throws ResourceException {
-        String iri = getRequest().getResourceRef().getPath().substring(10);
-        
+        String iri = getRequest().getResourceRef().getPath();
+        iri = iri.substring(iri.indexOf("metadata") + 9);
         this.datum = getMetadatum(getDataDir() + "/" + iri + ".xml");
     }
     
@@ -29,5 +37,48 @@ public class MDatumResource extends BaseResource {
         // Returns the XML representation of this document.
         return representation;
     }
-  
+    
+    @Get("html")
+    public Representation toHtml() {
+    	// Retrieve resource
+    	TransformerFactory tFactory = TransformerFactory.newInstance();
+    	
+    	StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("<html>");
+        stringBuilder
+                .append("<head><title>Metadata</title></head>");
+        stringBuilder.append("<body bgcolor=white>");
+    	
+        try {
+			 Transformer transformer =
+			  tFactory.newTransformer
+			     (new javax.xml.transform.stream.StreamSource
+			        (getClass().getResourceAsStream( "/rdf.xsl" )));
+			 
+			 StringWriter xmlOutWriter = new StringWriter();
+			 
+			 transformer.transform
+		      (new javax.xml.transform.stream.StreamSource
+		            (new StringReader(datum)),
+		       new javax.xml.transform.stream.StreamResult
+		            ( xmlOutWriter ));
+			 
+			 stringBuilder.append(xmlOutWriter.toString());
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	stringBuilder.append("</body>");
+        stringBuilder.append("</html>");
+        
+        Representation representation = (new StringRepresentation(stringBuilder
+                .toString(), MediaType.TEXT_HTML));
+		return representation;
+    }
+    
 }
