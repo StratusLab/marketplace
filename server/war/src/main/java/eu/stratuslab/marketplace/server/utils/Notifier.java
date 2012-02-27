9efb8d4734939fed4ff4836f04a9ac4bd6c2b96f
Stuart Kenny <stuart.kenny@scss.tcd.ie>
2012-02-27 12:12:25 +0000

    Copyright statement

--- server/war/src/main/java/eu/stratuslab/marketplace/server/utils/Notifier.java
+++ server/war/src/main/java/eu/stratuslab/marketplace/server/utils/Notifier.java
@@ -1,21 +1,21 @@
-/*
- Created as part of the StratusLab project (http://stratuslab.eu),
- co-funded by the European Commission under the Grant Agreement
- INSFO-RI-261552.
-
- Copyright (c) 2011, SixSq Sarl
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/**
+ * Created as part of the StratusLab project (http://stratuslab.eu),
+ * co-funded by the European Commission under the Grant Agreement
+ * INSFO-RI-261552.
+ *
+ * Copyright (c) 2011
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package eu.stratuslab.marketplace.server.utils;
 
