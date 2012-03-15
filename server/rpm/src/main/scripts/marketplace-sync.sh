aafd65c7908511fe29a75a814e610afdb1d7046f
Stuart Kenny <stuart.kenny@scss.tcd.ie>
2012-03-15 16:09:47 +0000

    Script for syncing Marketplace

---
+++ server/rpm/src/main/scripts/marketplace-sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONFIG=/etc/stratuslab/marketplace-sync.cfg
+
+MASTER=marketplace.stratuslab.eu
+SLAVE=http://localhost:8080
+DATADIR=/var/lib/stratuslab
+
+if [ -f $CONFIG ]; then
+    . $CONFIG
+fi
+
+/usr/bin/rsync -avz stkenny@$MASTER:/var/lib/stratuslab/* $DATADIR | grep .xml |sort > $DATADIR/sync
+
+curl $SLAVE/sync
